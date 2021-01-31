b5400dfdc8f5712ff59c4e5a7d2b20ac69a997d5
Copiadora Note <carlos010442@hotmail.com>
2021-01-31 10:20:28 -0300

    first commit

---
+++ app/models/produto.js
@@ -0,0 +1,28 @@
+/**
+ * Arquivo: produto.js
+ * Author: Glaucia Lemos
+ * Descrição: arquivo responsável onde trataremos o modelo da classe 'Produto'
+ * Data: 18/10/2017
+ * obs.: http://mongoosejs.com/docs/schematypes.html
+ */
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+/**
+ * Produto:
+ * 
+ * -> Id: int
+ * -> Nome: String
+ * -> Preco: Number
+ * -> Descricao: String
+ * 
+ */
+
+const ProdutoSchema = new Schema({
+    nome: String,
+    preco: Number,
+    descricao: String
+});
+
+module.exports = mongoose.model('Produto', ProdutoSchema);
